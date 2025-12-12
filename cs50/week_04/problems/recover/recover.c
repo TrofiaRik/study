6d205c8b8b3fa68efc6737c2bf4b360bfeb8a0ca
Riccardo Trofia Pavia <trofiarik@gmail.com>
2025-12-12 22:50:06 +0100

    debugged recover

--- cs50/week_04/problems/recover/recover.c
+++ cs50/week_04/problems/recover/recover.c
@@ -2,13 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* TO DO:
-*  1 open memory card
-*  2 look for the begining of jpg file
-*  3 open a new jpeg file where to store the found data
-*  4 write blocks of 512 bytes, until a new jpeg is found
-*/ 
-
 int main(int argc, char *argv[])
 {
     // Accept a single command-line argument
@@ -17,28 +10,24 @@ int main(int argc, char *argv[])
         printf("Usage: ./recover FILENAME\n");
         return 1;
     }
-    // Open the memory card
+    // Open the memory card & declare vars
     FILE *card = fopen(argv[1], "r");
     FILE *n_file;
     char filename[8];
-    // The data buffer for the date being read must be created before calling fread
-    uint8_t data_buffer[512];
-
-    // create index for 
+    uint8_t data_buffer[512]; //-> bef. fread, decl&assign databuffer
+    int idx = 0; //-> filename index
 
     // While there's still data left to read from the memory card
-    int idx = 0;
-
     while (fread(data_buffer, 1, 512, card) == 512)
     {
-        fread(data_buffer, 1, 512, card);
+        //fread(data_buffer, 1, 512, card);
         if (data_buffer[0] == 0xff && data_buffer[1] == 0xd8 &&
-            data_buffer[2] == 0xff && (data_buffer[3] & 0xf0) == 0xe0)
+            data_buffer[2] == 0xff && ((data_buffer[3] & 0xf0) == 0xe0))
         {       
             if (idx == 0)
             {    
                 sprintf(filename, "%03i.jpg", idx);
-                FILE *n_file = fopen(filename, "w");
+                n_file = fopen(filename, "w");
                 if (n_file == NULL)
                     return 1;
                 fwrite(data_buffer, 1, 512, n_file);
@@ -47,8 +36,9 @@ int main(int argc, char *argv[])
             else 
             {
                 fclose(n_file);
+                n_file = NULL;
                 sprintf(filename, "%03i.jpg", idx);
-                FILE *n_file = fopen(filename, "w");
+                n_file = fopen(filename, "w");
                 if (n_file == NULL)
                     return 1;
                 fwrite(data_buffer, 1, 512, n_file);
@@ -64,5 +54,6 @@ int main(int argc, char *argv[])
         }    
     }
     // close the file
+    fclose(card);
     fclose(n_file);    
 }
